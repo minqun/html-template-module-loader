2a65abcd8067fcacb5361b2e1f42926bdfb33fe9
M.re <undefined>
2024-02-21 16:20:14 +0800

    feat: 更新

--- sources/index.js
+++ sources/index.js
@@ -1,12 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const parseUtls = require('parse5-utils');
-const deafultOptions = {
-    tag: 'tpl',
-    attr: 'file',
-    tplTag: 'template',
-    tplAttr: 'tpl'
-}
+
 function findNode(node, tag, attrName, result) {
     if (node.tagName === tag && node.attrs) {
         const attr = node.attrs.find((attr) => attr.name === attrName);
@@ -20,28 +15,65 @@ function findNode(node, tag, attrName, result) {
         }
     }
 }
+function replaceTplData(node, data) {
+    if (node.nodeName == "#text") {
+        const getDataKey = [];
+        const pattern = /{{\s*([^{}^\s]*)\s*}}/g
+        let match;
+
+        while ((match = pattern.exec(node.value)) !== null) {
+            // 去掉空格
+            const trimmedMatch = match[1]
+            getDataKey.push(trimmedMatch);
+        }
+        getDataKey.forEach(item => {
+            let getDatakeyValue = data.match(new RegExp(`\"${item}\":[ ]{0,}\"([^\",]*)\"`, 'g'))[0] || ''
+            let getDataValue = getDatakeyValue.replace(new RegExp(`\"${item}\":[ ]{0,}\"(.*)\"`), '$1')
+            node.value = node.value.replace(new RegExp(`{{${item}}}|{{ ${item} }}`, 'g'), getDataValue)
+        })
+    }
+    if (node.childNodes) {
+        for (const childNode of node.childNodes) {
+            replaceTplData(childNode, data);
+        }
+    }
+}
+function findData(node, tag, attrName) {
+    if (node.tagName === tag && node.attrs) {
+        const data = node.attrs.find((attr) => attr.name === 'data');
+        return data.value
+    }
+   
+}
 function getFileNode(file) {
     const content = fs.readFileSync(file, 'utf8')
     return parseUtls.parse(content)
 }
-function insertFileNodeToTargetNode (node, insertFile, insertTag, insertAttr, _this) {
-    const options = Object.assign({}, deafultOptions,  _this.getOptions());
+
+function insertFileNodeToTargetNode (node, insertFile, insertTag, insertAttr, _this, data) {
+    const options = _this.getOptions();
     const result = []
     const insertNode = getFileNode(insertFile)
     replaceTplToHtml(insertNode, options.tag, options.attr, _this)
-    findNode(insertNode, options.tplTag, options.tplAttr, result)
+    findNode(insertNode, 'div', 'tpl', result)
+    if (data) {
+        console.log(result, 'result', data)
+        replaceTplData(insertNode,  data)
+    }
+   
     let newNode = parseUtls.createNode('div')
     newNode.childNodes = result[0].childNodes
-    console.log(result, '---result---')
+   
     parseUtls.replace(node, newNode)
 }
 function replaceTplToHtml(node, tag, attrName, _this) {
-    const options = Object.assign({}, deafultOptions,  _this.getOptions());
+    const options = _this.getOptions();
     if (node.tagName === tag) {
         const src = node.attrs.find(attr => attr.name == options.attr).value;
+        const data = node.attrs.find(attr => attr.name == 'data')?.value;
         if (src) {
             _this.addDependency(path.resolve(src));
-            insertFileNodeToTargetNode(node, path.resolve(src), options.tplTag, options.tplAttr, _this)
+            insertFileNodeToTargetNode(node, path.resolve(src), 'div', 'tpl', _this, data)
         }
     }
     if (node.childNodes) {
@@ -52,11 +84,13 @@ function replaceTplToHtml(node, tag, attrName, _this) {
 }
 
 module.exports = async function (source) {
-    const options = Object.assign({}, deafultOptions,  this.getOptions());
+    const options = this.getOptions();
     var callback = this.async();
     var document = parseUtls.parse(source)
     replaceTplToHtml(document, options.tag, options.attr, this)
     const html = parseUtls.serialize(document)
+   
     callback(null, html)
+    this.cacheable(false)
 }
 module.exports.raw = false;
